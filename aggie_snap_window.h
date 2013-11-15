7b82d46d3271f8b73fccc80bbd7f0aa1f2f0f140
Alex Benavides <gowroo@hotmail.com>
2013-11-14 20:16:35 -0600

    Update aggie_snap_window.h

--- aggie_snap_window.h
+++ aggie_snap_window.h
@@ -6,54 +6,63 @@
 #include "Window.h"
 
 using namespace Graph_lib;
+using namespace stdl
 
 //------------------------------------------------------------------------------
 
 struct aggie_snap_window : Graph_lib::Window {
     aggie_snap_window(Point xy, int w, int h, const string& title );
-	
-	Menu files_menu;
+        
+        Menu files_menu;
     Button files_button;
-	
-	Text Intro;
-	Text Description;
-	Text Names;
-	
-	In_box add_box;
-	Button add_button;
-	
-	In_box add_url_image_box;
-	Button add_url_image_button;
-	
-	In_box search_box;
-	Button search_button;
-	
-	
+        
+        Text Intro;
+        Text Description;
+        Text Names;
+        
+        
 private:
+        In_box add_box;
+        Button add_button;
+        
+        In_box add_url_image_box;
+        Button add_url_image_button;
+        
+        In_box search_box;
+        Button search_button;
+		
+		Out_box answer_out;
+        
+        void hide_files() { files_menu.hide();} 
+        void files_pressed() { files_button.hide(); files_menu.show(); detach(Intro); detach(Description); detach(Names);}
+        void show_in_box() { attach(add_box); attach(add_button); attach(answer_out); }
+        void show_search_box() { attach(search_box); attach(search_button); }        
+        void show_url_input() { attach(add_url_image_box); attach(add_url_image_button); }              
+        void search_pressed() {detach(search_box); detach(search_button); files_button.show();}                                //function that is called when the search BUTTON is pressed
+        void add_pressed() { detach(add_box);
+							 detach(add_button);
+							 files_button.show();
+							 string file = add_box.get_string();
+							 ostringstream os;
+						     os << file;
+							 answer_out.put(os.str()); 
+							}                                        //function that is called when the add BUTTON is pressed
+        void url_pressed() { files_button.show(); detach(add_url_image_box); detach(add_url_image_button);} //function that is called when the url add BUTTON is pressed
+        
+        static void cb_add(Address, Address);                                         // callback for add_picture
+        static void cb_search(Address, Address);                                 // callback for search_picture
+        static void cb_files(Address, Address);
+		static void cb_answer(Address, Address);
+        static void cb_url_input(Address, Address);
+        static void cb_url_pressed(Address, Address);
+        static void cb_picture_search(Address, Address);
+        static void cb_picture_add(Address, Address);
+        
+        void add(){ hide_files(); show_in_box();}
+        void search(){ hide_files(); show_search_box();}
+        void url_input() { hide_files(); show_url_input(); }
 
-	
-	void hide_files() { files_menu.hide();} 
-	void files_pressed() { files_button.hide(); files_menu.show(); detach(Intro); detach(Description); detach(Names);}
-	void show_in_box() { attach(add_box); attach(add_button);}
-	void show_search_box() { attach(search_box); attach(search_button); }        
-	void show_url_input() { attach(add_url_image_box); attach(add_url_image_button); }              
-	void search_pressed() {detach(search_box); detach(search_button); files_button.show();}				//function that is called when the search BUTTON is pressed
-	void add_pressed() { detach(add_box); detach(add_button);  files_button.show(); }					//function that is called when the add BUTTON is pressed
-	void url_pressed() { files_button.show(); detach(add_url_image_box); detach(add_url_image_button);} //function that is called when the url add BUTTON is pressed
-	
-	static void cb_add(Address, Address);		 			// callback for add_picture
-	static void cb_search(Address, Address);				 // callback for search_picture
-	static void cb_files(Address, Address);
-	static void cb_url_input(Address, Address);
-	static void cb_url_pressed(Address, Address);
-	static void cb_picture_search(Address, Address);
-	static void cb_picture_add(Address, Address);
-	
-	void add(){ hide_files(); show_in_box();}
-	void search(){ hide_files(); show_search_box();}
-	void url_input() { hide_files(); show_url_input(); }
-
-	
+        
 };
 
 //------------------------------------------------------------------------------
