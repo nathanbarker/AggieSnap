bbe6f104521e38893a0df45b686e580cd752c5ce
nathanbarker <nbarker@outlook.com>
2013-11-20 20:55:55 -0600

    Update - Browse Added
    BUGS -
    1. If Browse is hit before there any images it will exit
    2. Next button does not detach
    3. previous button also has trouble detaching
    4. there is no check for files that could not be downloaded/copied

--- aggie_snap_window.cpp
+++ aggie_snap_window.cpp
@@ -12,14 +12,16 @@ aggie_snap_window::aggie_snap_window(Point xy, int w, int h, const string& title
 				Intro(Point(350,250),"This program is a photo library."),
                 Description(Point(275,270),"It's features include adding and searching pictures."),
                 Names(Point(0,590),"Designed by: Alex Benavides, Nathan Barker, Ezekiel Cabezas."),
-                Url_Error(Point(505,20),"Bad Input. Check the address and tags."),
+                Url_Error(Point(505,20),"Bad Input. Check the address, tags and there is a name."),
 				Tag_Error(Point(505,70),"(only Family, Friends, Aggieland, Pets, Vacation)"),
 				eg_tag(Point(505,70),"(e.g. Family, Friends, Aggieland, Pets, Vacation)"),
 				eg_url(Point(505,20),"(e.g. http://Aggielandrules.com/picture.jpg)"),
-				eg_local(Point(505,25),"(e.g. \\Pictures\\folder1\\Reveille.gif)"),
-			
+				eg_local(Point(505,25),"(e.g. /Pictures/folder1/Reveille.gif)"),
+				
+				
                 files_button(Point(0,0),70,20,"Files",cb_files),
                 files_menu(Point(0,20),70,20,Menu::vertical,"File"),
+				
                 
                 start_button(Point(0,0),1000,600,"Start ", cb_start_pressed),
 				search_box(Point(150,5),200,20,"Search:"),
@@ -31,12 +33,15 @@ aggie_snap_window::aggie_snap_window(Point xy, int w, int h, const string& title
                 add_url_image_button(Point(355,30),70,20, "Add", cb_url_pressed),
 				close_web_add(Point(430,30),70,20, "Close", cb_close_url_add),
 				tag_box(Point(200,55),300,20, "Add Tags:"),
-                name_box(Point(200,30),150,20,"Name your picture:")
+                name_box(Point(200,30),150,20,"Name your picture:"),
+				next_button(Point(900,500),70,20, "Next", cb_next_image),
+				previous_button(Point(100,500),70,20, "Previous", cb_previous_image)
         {
                 files_menu.attach(new Button(Point(0,0),0,0,"Local Add",cb_add));
                 files_menu.attach(new Button(Point(0,0),0,0,"Web Add",cb_url_input)); 
                 files_menu.attach(new Button(Point(0,0),0,0,"Search",cb_search));
-                
+                files_menu.attach(new Button(Point(0,0),0,0,"Browse",cb_browse));
+				
                 attach(files_menu);
                 attach(files_button);
                 files_menu.hide();
@@ -63,6 +68,9 @@ aggie_snap_window::aggie_snap_window(Point xy, int w, int h, const string& title
         }
 
 
+		
+		
+		
 //------------------------------------------------------------------------------
 
 void aggie_snap_window::cb_add(Address, Address pw)     // "the usual"
@@ -126,6 +134,26 @@ void aggie_snap_window::cb_close_url_add(Address, Address pw)     // "the usual"
 void aggie_snap_window::cb_start_pressed(Address, Address pw)     // "the usual"
 {  
     reference_to<aggie_snap_window>(pw).start_pressed();
+} 
+
+//------------------------------------------------------------------------------
+void aggie_snap_window::cb_browse(Address, Address pw)     // "the usual"
+{  
+    reference_to<aggie_snap_window>(pw).img_browse();
+} 
+
+//------------------------------------------------------------------------------
+
+void aggie_snap_window::cb_next_image(Address, Address pw)     // "the usual"
+{  
+    reference_to<aggie_snap_window>(pw).next_image();
+} 
+
+//------------------------------------------------------------------------------
+
+void aggie_snap_window::cb_previous_image(Address, Address pw)     // "the usual"
+{  
+    reference_to<aggie_snap_window>(pw).previous_image();
 } 
 
 //------------------------------------------------------------------------------
--- aggie_snap_window.h
+++ aggie_snap_window.h
@@ -32,9 +32,13 @@ struct aggie_snap_window : Graph_lib::Window {
 		ofstream output;
         ifstream input;
 		
-		int count;
+		int count = 0;
 		ofstream count_add;
 		ifstream count_in;
+		
+		ifstream browser;
+		Image* display_img;
+	
         
 private:
         In_box add_box;
@@ -53,13 +57,18 @@ private:
 		Button close_web_add;
 		In_box tag_box;
 		
+		Vector< Vector<string> > matrix;
 		Vector <string> v;
         Vector <string> transfer;
 		
-		void start_pressed() {detach(Start); detach(start_button); attach(AggieSnap); picture_count();}   
+		Button next_button;
+		Button previous_button;
+		
+		void start_pressed() {detach(Start); detach(start_button); attach(AggieSnap);}   
         void hide_files() { files_menu.hide();} 
         void files_pressed() { files_button.hide(); files_menu.show(); detach(Intro); detach(Description); detach(Names);}
-        
+        void img_browse() { files_menu.hide(); browse(); attach(next_button);}
+		
 		void show_in_box() { attach(add_box); attach(add_button); attach(name_box); attach(close_add); attach(tag_box); attach(eg_tag); attach(eg_local);}
         void show_search_box() { attach(search_box); attach(search_button); }        
                      
@@ -71,7 +80,9 @@ private:
 		void show_url_input() { attach(add_url_image_box); attach(add_url_image_button); attach(name_box); attach(close_web_add); attach(tag_box); attach(eg_tag); attach(eg_url); } 																				
 		void close_url_add() { files_button.show(); detach(add_url_image_box); detach(add_url_image_button); detach(name_box); detach(close_web_add); detach(tag_box); detach(Url_Error); detach(Tag_Error); detach(eg_tag); detach(eg_url); redraw();}
 		
+		
 	
+		static void cb_browse(Address, Address);
 		static void cb_start_pressed(Address, Address);
         static void cb_add(Address, Address);                                         // callback for add_picture
         static void cb_add_close(Address, Address);
@@ -81,145 +92,29 @@ private:
         static void cb_url_pressed(Address, Address);
         static void cb_picture_search(Address, Address);
         static void cb_picture_add(Address, Address);
-		static void cb_close_url_add(Address, Address);
-        
+		static void cb_close_url_add(Address, Address); 
+        static void cb_next_image(Address, Address);
+		static void cb_previous_image(Address, Address);
+		
         void add(){ hide_files(); show_in_box();}
         void search(){ hide_files(); show_search_box();}
         void url_input() { hide_files(); show_url_input(); }
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------	
-		
-		void picture_count()
-		{
-			count_in.open("Picture_Count.txt");
-			string move;
-			getline(count_in, move);
-			count_in.close();
-			
-			count = stoi(move);
-		}
-		
-		void inc_count()
-		{
-			count = count + 1;
-			count_add.open ("Picture_Count.txt");	
-			count_add<<count;
-			output.close();
-		}
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-		void next_url()
-		{
-			string img_name;
-			string url;
-			string final_url;
-			string bgning_string;
-			string ending_string;	
 
-			url=add_url_image_box.get_string();
-			if(url != "")
-			{
-				bgning_string=url.substr(0,7);
-				ending_string=url.substr(url.size()-4,url.size()-1);
-		
-				img_name=name_box.get_string();
-				if(img_name == "")
-				{
-					img_name = static_cast<ostringstream*>( &(ostringstream() << count) )->str();
-					inc_count();
-				}
-				
-				bool check = false;
-			
-				string tags;
-				tags=tag_box.get_string();
-				istringstream ss(tags);
-				string token;
-				v.clear();
-				while(getline(ss, token, ','))
-				{
-					v.push_back(token);
-				}				
-				for(int i = 0; i<v.size(); ++i)
-				{
-					check=false;
-					if(v[i]=="Family" || v[i]=="Friends" || v[i]=="Aggieland" || v[i]=="Pets" || v[i]=="Vacation")
-					{check=true;}
-				} 
-				if(	bgning_string=="http://" &&	
-				   (ending_string==".jpg" || 
-					ending_string==".gif" ||
-					ending_string==".JPG" ||
-					ending_string==".GIF" ||
-					ending_string==".jpeg"||			
-					ending_string==".JPEG" )
-					&& check == true && v.size()<6)
-			
-				{
-					final_url= "wget -O Images/"+img_name+ending_string+" "+url;
-				
-					system(final_url.c_str());
-				
-					input.open("Library.txt");
-					string move;
-					transfer.clear();
-					while(getline(input, move))
-					{	
-						transfer.push_back(move);
-					}
-					input.close();	
-				
-					output.open ("Library.txt");
-				
-					for(int i=0; i<transfer.size(); ++i)
-					{
-						output<<transfer[i]<<endl;
-					}
-				
-					output<<"("+img_name+".jpg";			
-					for(int i=0; i<v.size(); ++i)
-					{
-						output<<","+v[i];
-					}
-					output<<")";
-					output.close();
-				}
-				else
-				{
-					attach(Url_Error);
-					attach(Tag_Error);
-					redraw();
-				}
-			}
-			else
-			{
-				attach(Url_Error);
-				attach(Tag_Error);
-				redraw();
-			}
-			
-		}
-//-------------------------------------------------------------------------------------
-    void next_local()
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+	void next_url()
 	{
 		string img_name;
-		string location;
-		string final_location;
+		string url;
+		string final_url;
 		string bgning_string;
-		string ending_string;
+		string ending_string;	
 
-		location=add_box.get_string();
-		if(location != "")
+		url=add_url_image_box.get_string();
+		img_name=name_box.get_string();
+		if(url != "" || img_name != "" )
 		{
-			bgning_string=location.substr(1,2)+" ";
-			ending_string=location.substr(location.size()-4,location.size()-1);
-	
-			img_name=name_box.get_string();
-
-			if(img_name == "")
-			{
-				img_name = static_cast<ostringstream*>( &(ostringstream() << count) )->str();
-				inc_count();
-			}
-			
+			bgning_string=url.substr(0,7);
+			ending_string=url.substr(url.size()-4,url.size()-1);
 			
 			bool check = false;
 		
@@ -228,7 +123,95 @@ private:
 			istringstream ss(tags);
 			string token;
 			v.clear();
-			while(getline(ss, token, ','))
+			while(ss>>token)
+			{
+				v.push_back(token);
+			}				
+			for(int i = 0; i<v.size(); ++i)
+			{
+				check=false;
+				if(v[i]=="Family" || v[i]=="Friends" || v[i]=="Aggieland" || v[i]=="Pets" || v[i]=="Vacation")
+				{check=true;}
+			} 
+			if(	bgning_string=="http://" &&	
+			   (ending_string==".jpg" || 
+				ending_string==".gif" ||
+				ending_string==".JPG" ||
+				ending_string==".GIF" ||
+				ending_string==".jpeg"||			
+				ending_string==".JPEG" )
+				&& check == true && v.size()<6)
+		
+			{
+				final_url= "wget -O Images/"+img_name+ending_string+" "+url;
+			
+				system(final_url.c_str());
+			
+				input.open("Library.txt");
+				string move;
+				transfer.clear();
+				while(getline(input, move))
+				{	
+					transfer.push_back(move);
+				}
+				input.close();	
+			
+				output.open ("Library.txt");
+			
+				for(int i=0; i<transfer.size(); ++i)
+				{
+					output<<transfer[i]<<endl;
+				}
+			
+				output<<img_name+ending_string;			
+				for(int i=0; i<5; ++i)
+				{
+					if(i<v.size())
+					{
+						output<<" "+v[i];
+					}
+					else
+					{
+						output<<" x";
+					}
+				}
+				output.close();
+			}
+			else
+			{
+				attach(Url_Error);
+				attach(Tag_Error);
+				redraw();
+			}
+		}
+		else
+		{
+			attach(Url_Error);
+			attach(Tag_Error);
+			redraw();
+		}
+		
+	}
+//-------------------------------------------------------------------------------------
+    void next_local()
+	{
+		string img_name;
+		string location;
+		string final_location;
+		string ending_string;
+
+		location=add_box.get_string();
+		img_name=name_box.get_string();
+		if(location != "" || img_name != "" )
+		{
+			ending_string=location.substr(location.size()-4,location.size()-1);
+			bool check = false;
+			string tags;
+			tags=tag_box.get_string();
+			istringstream ss(tags);
+			string token;
+			v.clear();
+			while(ss>>token)
 			{
 				v.push_back(token);
 			}				
@@ -246,7 +229,6 @@ private:
 				ending_string==".jpeg"||			
 				ending_string==".JPEG" )
 				&& check == true && v.size()<6)
-		
 			{
 				final_location= "cp ~/"+location+" Images/"+img_name+ending_string;
 			
@@ -267,13 +249,18 @@ private:
 				{
 					output<<transfer[i]<<endl;
 				}
-			
-				output<<"("+img_name+".jpg";			//Family, Friends, Aggieland, Pets, Vacation
-				for(int i=0; i<v.size(); ++i)
+				output<<img_name+ending_string;			//Family, Friends, Aggieland, Pets, Vacation
+				for(int i=0; i<5; ++i)
 				{
-					output<<","+v[i];
+					if(i<v.size())
+					{
+						output<<" "+v[i];
+					}
+					else
+					{
+						output<<" x";
+					}
 				}
-				output<<")";
 				output.close();
 			}
 			else
@@ -293,8 +280,78 @@ private:
 				
 	}
   
-};
 
 //------------------------------------------------------------------------------
 
+	void browse()
+	{
+		count = 0;
+		matrix.clear();
+		string token,tag1, tag2, tag3, tag4, tag5;
+		browser.open("Library.txt");
+
+		while(browser>>token>>tag1>>tag2>>tag3>>tag4>>tag5)
+		{	
+			vector <string> row;
+			row.push_back(token);
+			if (tag1 != "x")
+			{
+				row.push_back(tag1);
+			}
+			if (tag2 != "x")
+			{
+				row.push_back(tag2);
+			}
+			if (tag3 != "x")
+			{
+				row.push_back(tag3);
+			}
+			if (tag4 != "x")
+			{
+				row.push_back(tag4);
+			}
+			if (tag5 != "x")
+			{
+				row.push_back(tag5);
+			}
+			matrix.push_back(row);
+		}
+		browser.close();	
+
+		display_img = new Image ( Point(100,100), "Images/"+matrix[0][0] );
+		attach(*display_img);
+		redraw();
+	}	
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------	
+	void next_image()
+	{
+		++count;
+		if(count == matrix.size())
+		{
+			detach(next_button);
+		}
+		attach(previous_button);
+		detach(*display_img);
+		display_img = new Image ( Point(100,100), "Images/"+matrix[count][0]);
+		attach(*display_img);
+		redraw();
+	}
+	
+//-----------------------------------------------------------------------------------------------------
+	void previous_image()
+	{
+		--count;
+		if(count == 0)
+		{
+			detach(previous_button);
+		}
+		detach(*display_img);
+		display_img = new Image ( Point(100,100), "Images/"+matrix[count][0]);
+		attach(*display_img);
+		redraw();
+	}
+
+//-----------------------------------------------------------------------------------------------------
+
+};
 #endif // AGGIE_WINDOW_GUARD
